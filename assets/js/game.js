fc41585258ed1ac87a96bdf473d456866fc2d38e
John Phillips <jphillips37@gmail.com>
2018-11-09 18:20:15 -0500

    finished game

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,6 +36,7 @@ var word = game.guessWord.replace(/\w/g, "_"); // use a RegEx to replace all let
             
 console.log(word);
 
+
 function updateAll() {
     winsID.textContent = "Wins: " + game.wins;
     lossesID.textContent = "Losses: " + game.losses;
@@ -51,39 +52,64 @@ var userLetter = "";
 var wordArray = game.guessWord.split(""); // array of letters from the word being guessed
 var guessWordArray = word.split(""); // array of word as it appears to the user
 
+function resetAll() {
+    // randomWord() gets a new word and resets the number of guesses
+    randomWord();
+    // reset word to all "_"
+    word = game.guessWord.replace(/\w/g, "_");
+    // reset lettersGuessed array
+    game.lettersGuessed = [];
+    // reset arrays
+    wordArray = game.guessWord.split("");
+    guessWordArray = word.split("");
+    //updateAll()
+    updateAll();
+}
+
 document.onkeyup = function(event) {
     userLetter = event.key; // save user input
     userLetter = userLetter.toUpperCase(); // make input upper case
     
     // make sure userLetter is a letter, check number of guesses remaining, user guessed has not already been guessed, and check that guess word is not complete
-    if (userLetter.match(/^[A-Za-z]+$/) && game.remGuesses > 0 && game.lettersGuessed.indexOf(userLetter) == -1 && word.includes("_") == true) { 
+    if (userLetter.match(/^[A-Za-z]+$/) && game.remGuesses > 0 && game.lettersGuessed.indexOf(userLetter) == -1 && wordGuessID.textContent.includes("_") == true) { 
         
         console.log(userLetter);
         console.log(game.guessWord.length);
         // check string for userLetter. if it exists, replace the "_" with the letter
         if (game.guessWord.search(userLetter) != -1) {  //search the array to see if the user letter exists in the guess word
-            for (i = 0; i < wordArray.length; i++) {
+            for (i = 0; i < wordArray.length; i++) {  // if the letter is correct, put it in the right spot
                 
-                if (wordArray[i] === userLetter) {
+                if (wordArray[i] === userLetter) { // userLetter is equal to the letter at index i
                     guessWordArray[i] = userLetter;  // it seems easier to change elements in an array rather than characters in a string
                     console.log(guessWordArray);
                     word = guessWordArray.join("");  // concatenate all values of the array to put everything back into a single string
+                    updateAll();
+                    
+                    if (word.includes("_") == false) {
+                        game.wins++;
+                        updateAll();
+                        alert("Contratulations! You have guessed the word!");
+                        // resetAll();
+                    }
                 }
                 else {
                     // do nothing here because the guessed letter doesn't belong at this index
                 }
             }
         }
-        else {
+        else { //the user letter is not in the guess word
             game.lettersGuessed.push(userLetter); // if userLetter is not in word, then add it to game.lettersGuessed and decrement game.remguesses
             game.remGuesses--; // decrement remaining guesses if incorrect guess
+            updateAll();
                 if (game.remGuesses == 0) {
                     word = game.guessWord
-                    alert("You Lose");
                     game.losses++;
+                    updateAll();
+                    alert("You Lose");
+                    
                 }
         }   
-        updateAll();
+        // updateAll();
         console.log(game.remGuesses);
         
         
@@ -93,7 +119,10 @@ document.onkeyup = function(event) {
             // not a letter, ignore input
         }
         else if (game.remGuesses == 0) {
-            
+            // no guesses remaining, do nothing
+        }
+        else if (game.lettersGuessed.indexOf(userLetter) >= 0){
+
         }
     }
 };
